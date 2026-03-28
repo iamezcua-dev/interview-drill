867820c4841cf8569414ef50a16f77608f97d470
Eddie Jobs <iamezcua.dev@gmail.com>
2026-03-28 12:38:23 -0600

    Q15 - SQL
    - Return all departments with their employee count, including departments with 0 employees.

---
+++ sql/dml/q15_solution.sql
@@ -0,0 +1,26 @@
+-- Q15 [SQL | Coding | Senior]
+-- Return all departments with their employee count,
+-- including departments that have no employees (count = 0).
+--
+-- Tables: dept, emp
+-- dept  : department_id    INT
+--         department_name  VARCHAR(50)
+-- emp   : employee_id      INT
+--         department_id    INT NULL
+-- Output: department_name, employee_count
+
+USE UnoSquareDrill;
+GO
+
+SET NOCOUNT ON;
+GO
+
+CREATE OR ALTER PROCEDURE dbo.q15_solution
+AS
+BEGIN
+
+    -- solution goes here
+    RETURN
+
+END;
+GO
