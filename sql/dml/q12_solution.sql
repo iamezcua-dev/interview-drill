8afee71fa0dd362e7938ca955ebb1bbb37d12a4e
Eddie Jobs <iamezcua.dev@gmail.com>
2026-03-28 12:37:39 -0600

    Q12 - SQL
    - Find the top 3 customers by total amount spent. Return customer_id and total_spent, ordered by total_spent descending. If there is a tie for 3rd place,
    include all tied customers.

---
+++ sql/dml/q12_solution.sql
@@ -0,0 +1,27 @@
+-- Q12 [SQL | Coding | Senior]
+-- Find the top 3 customers by total amount spent.
+-- Return customer_id and total_spent, ordered by total_spent desc.
+-- If there is a tie for 3rd place, include all tied customers.
+--
+-- Table : orders
+-- Cols  : order_id    INT
+--         customer_id INT
+--         order_date  DATE
+--         amount      DECIMAL(10,2)
+-- Output: customer_id, total_spent
+
+USE UnoSquareDrill;
+GO
+
+SET NOCOUNT ON;
+GO
+
+CREATE OR ALTER PROCEDURE dbo.q12_solution
+AS
+BEGIN
+
+    -- solution goes here
+    RETURN
+
+END;
+GO
