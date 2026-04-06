9e0425d7e9eacb486b47ff496ff7c6ed1c02cfd7
Natan Perusquia <astro_nat@outlook.com>
2026-04-06 03:54:48 -0600

    Natan solution - pyspark problem 4

--- src/main/python/q4_solution.py
+++ src/main/python/q4_solution.py
@@ -2,7 +2,14 @@
 # Output columns: employee_id, name, department, salary.
 
 from pyspark.sql import DataFrame
-
+from pyspark.sql.functions import avg
 
 def solution(df: DataFrame) -> DataFrame:
-    pass
+    averages_df = (
+        df.groupBy("department")
+        .agg(
+            avg("salary").alias("avg_dept_salary")
+        )
+    )
+    result = df.join(averages_df, "department").filter(df.salary > averages_df.avg_dept_salary)
+    return result.select(result.employee_id, result.name, result.department, result.salary)
